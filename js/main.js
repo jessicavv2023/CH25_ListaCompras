954f8eb1f74e96109ee5294f7ade4898c4da6030
valadezjessica <jessica.jessicavaladezv@gmail.com>
2023-03-29 16:22:20 -0600

    agregue txtNombre

--- js/main.js
+++ js/main.js
@@ -13,9 +13,15 @@ btnClear.addEventListener("click", function (event) {
 //
 btnAgregar.addEventListener("click", function (event) {
   event.preventDefault();
+  txtNombre.value = txtNombre.value.trim();
   if (txtNombre.value.length == 0) {
     txtNombre.style.border = "solid thin reds";
   } else {
-    txtNombre.style.border = "solid thin reds";
+    txtNombre.style.border = "";
+  }
+  if (txtNumber.value.length == 0) {
+    txtNumber.style.border = "solid thin reds";
+  } else {
+    txtNumber.style.border = "";
   }
 });
