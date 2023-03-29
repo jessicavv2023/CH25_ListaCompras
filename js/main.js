b01fccfa510c1ba93ed09196921c5d32b3ac5b6b
valadezjessica <jessica.jessicavaladezv@gmail.com>
2023-03-29 16:17:42 -0600

    agregue las primeras lineas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-// El código va aquí -> 
+let txtNombre = document.getElementById("Name");
+
+let txtNumber = document.getElementById("Number");
+let btnAgregar = document.getElementById("btnAgregar");
+let btnClear = document.getElementById("btnClear");
+
+//limpiar campos
+btnClear.addEventListener("click", function (event) {
+  event.preventDefault();
+  txtNombre.value = "";
+  txtNumber.value = "";
+});
+//
+btnAgregar.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (txtNombre.value.length == 0) {
+    txtNombre.style.border = "solid thin reds";
+  } else {
+    txtNombre.style.border = "solid thin reds";
+  }
+});
